6aecd0d127bee48c68c18bc390e07d29fc5d4f86
zhihui.zhou <zhihui.zhou@anker.io>
2024-06-27 21:06:35 +0800

    ci: first commit

---
+++ app/lbw/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function index() {
+  return (
+    <><div className='w-4 h-4 text-blue-200'>lbwnb666</div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold linear-gradient(to right, #6A1B9A, #FF8E53) bg-clip-text text-transparent text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-orange-400">
+        哈哈我要吃饭
+      </h1>
+    </div>
+    <div className="space-y-4">
+  <div className="w-96 bg-white shadow rounded">
+      w-96
+  </div>
+  <div className="w-80 bg-white shadow rounded">
+      w-80
+  </div>
+  <div className="w-72 bg-white shadow rounded">
+      w-72
+  </div>
+  <div className="w-64 bg-white shadow rounded">
+      w-64
+  </div>
+  <div className="w-60 bg-white shadow rounded">
+      w-60
+  </div>
+  <div className="w-56 bg-white shadow rounded">
+      w-56
+  </div>
+  <div className="w-52 bg-white shadow rounded">
+      w-52
+  </div>
+  <div className="w-48 bg-white shadow rounded">
+      w-48
+  </div>
+</div>
+
+    
+    </>
+    
+    
+   
+  )
+}
+
+export default index
